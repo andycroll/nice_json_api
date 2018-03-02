db540c8753bd33b9567699a349591261bba8293c
Andy Croll <andy@goodscary.com>
2018-03-02 12:05:46 +0000

    Add simplecov

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,5 +1,10 @@
-$LOAD_PATH.unshift File.expand_path("../../lib", __FILE__)
-require "nice_json_api"
+require 'simplecov'
+SimpleCov.start do
+  add_filter '/test/'
+end
 
-require "minitest/autorun"
+$LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
+require 'nice_json_api'
+
+require 'minitest/autorun'
 require 'webmock/minitest'
